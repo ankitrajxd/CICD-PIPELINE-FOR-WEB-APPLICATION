70bb0ea7defa3cf5007284d51d27f0b2769c7583
ankitraz <109873375+ankitraz@users.noreply.github.com>
2023-02-11 16:54:01 +0530

    updated groovy script

--- script.groovy
+++ script.groovy
@@ -1,7 +1,7 @@
 def Build(){
     withCredentials([usernamePassword(credentialsId: 'dockerhub-id-pass', usernameVariable: 'DOCKER_USER', passwordVariable: 'DOCKER_PASS')]) {
-        sh "docker build -t mywebsite:1.0 ."
         sh 'docker login -u $DOCKER_USER -p $DOCKER_PASS'
+        sh "docker build -t mywebsite:1.0 ."
         sh "docker push ankitraz/mywebsite:1.0"
     }
 }
