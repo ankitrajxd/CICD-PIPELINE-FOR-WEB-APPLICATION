373feac72830788043ed57c8e690a077a7053705
ankitraz <109873375+ankitraz@users.noreply.github.com>
2023-02-12 12:10:47 +0530

    updated script

--- script.groovy
+++ script.groovy
@@ -2,11 +2,11 @@ def Build(){
     echo "Building the application..."
     withCredentials([usernamePassword(credentialsId: 'dockerhub-id-pass', usernameVariable: 'DOCKER_USER', passwordVariable: 'DOCKER_PASS')]) {
         sh 'docker login -u $DOCKER_USER -p $DOCKER_PASS'
-        sh "docker build -t mywebsite:1.1 ."
-        sh "docker tag mywebsite:1.0 ankitraz/mywebsite:1.1"
+        sh "docker build -t mywebsite:${params.Version} ."
+        sh "docker tag mywebsite:${params.Version} ankitraz/mywebsite:${params.Version}"
         /* if you want to push to dockerhub you need to build image with your repo name and tag.
         otherwise you"ll have to tag image manually like i did above.*/
-        sh "docker push ankitraz/mywebsite:1.0"
+        sh "docker push ankitraz/mywebsite:${params.Version}"
     }
 }
 
@@ -16,7 +16,7 @@ def Test(){
 
 def Deploy(){
     echo "Deploying the application..."
-    def dockerCmd = "docker run -d  -p 81:80 ankitraz/mywebsite:1.1"
+    def dockerCmd = "docker run -d  -p 82:80 ankitraz/mywebsite:${params.Version}"
     sshagent(['github-ssh-key']) {
         sh "ssh -o StrictHostKeyChecking=no root@206.189.140.139 ${dockerCmd}"
     }
