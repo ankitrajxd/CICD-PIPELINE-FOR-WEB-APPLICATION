137acd99491209862339850b616f7fcdc83c3893
ankitraz <109873375+ankitraz@users.noreply.github.com>
2023-02-25 22:53:35 +0530

    add docker-compose

--- script.groovy
+++ script.groovy
@@ -2,7 +2,7 @@ def Build(){
     echo "Building the application..."
     withCredentials([usernamePassword(credentialsId: 'dockerhub-id-pass', usernameVariable: 'DOCKER_USER', passwordVariable: 'DOCKER_PASS')]) {
         sh 'docker login -u $DOCKER_USER -p $DOCKER_PASS'
-        sh "docker build -t mywebsite:${env.TAG} ."
+        sh "docker-compose build"
         sh "docker tag mywebsite:${env.TAG} ankitraz/mywebsite:${env.TAG}"
         /* if you want to push to dockerhub you need to build image with your repo name and tag.
         otherwise you"ll have to tag image manually like i did above.*/
@@ -16,8 +16,9 @@ def Test(){
 
 def Deploy(){
     echo "Deploying the application..."
-    def dockerCmd = "docker run -d  -p ${env.PORT}:80 ankitraz/mywebsite:${env.TAG}"
+    def dockerCmd = "TAG=${env.TAG} docker-compose up -d"
     sshagent(['github-ssh-key']) {
+        sh "cd /new"
         sh "ssh -o StrictHostKeyChecking=no root@64.227.108.131 ${dockerCmd}"
     }
 }
