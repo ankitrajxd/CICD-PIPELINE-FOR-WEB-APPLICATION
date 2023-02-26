31267c07f373ba8b29b1f5045c916be1a0f39600
ankitraz <109873375+ankitraz@users.noreply.github.com>
2023-02-26 10:01:42 +0530

    added comments

--- script.groovy
+++ script.groovy
@@ -1,29 +1,29 @@
 def Build(){
     echo "Building the application..."
     withCredentials([usernamePassword(credentialsId: 'dockerhub-id-pass', usernameVariable: 'DOCKER_USER', passwordVariable: 'DOCKER_PASS')]) {
-        sh 'docker login -u $DOCKER_USER -p $DOCKER_PASS'
-        sh "docker build -t mywebsite:${env.TAG} ."
-        sh "docker tag mywebsite:${env.TAG} ankitraz/mywebsite:${env.TAG}"
+        sh 'docker login -u $DOCKER_USER -p $DOCKER_PASS'  // login to dockerhub
+        sh "docker build -t mywebsite:${env.TAG} ."  // build image with tag
+        sh "docker tag mywebsite:${env.TAG} ankitraz/mywebsite:${env.TAG}"  // tag image with your repo name
         /* if you want to push to dockerhub you need to build image with your repo name and tag.
         otherwise you"ll have to tag image manually like i did above.*/
-        sh "docker push ankitraz/mywebsite:${env.TAG}"
+        sh "docker push ankitraz/mywebsite:${env.TAG}"  // push image to dockerhub
     }
 }
 
 def Test(){
-    echo "Testing the application..."
+    echo "Testing the application..." // you can add your test cases here
 }
 
 def Deploy(){
     echo "Deploying the application..."
-    def makedir = "mkdir /root/new"
-    def cd = "cd /root/new"
-    def dockerCmd = "TAG=${env.TAG} docker-compose up -d"
+    def makedir = "mkdir /root/new"  // creating a new directory on remote server
+    def cd = "cd /root/new"  // changing directory to new directory on remote server where we copied docker-compose.yml file
+    def dockerCmd = "TAG=${env.TAG} docker-compose up -d" // running docker-compose up command on remote server using TAG variable
     
     sshagent(['github-ssh-key']) {
         //copying docker-compose.yml file to remote server
-        sh "scp docker-compose.yml root@64.227.108.131:/root/new"
-        sh "ssh -o StrictHostKeyChecking=no root@64.227.108.131 '${makedir};${cd};${dockerCmd}'"
+        sh "scp docker-compose.yml root@64.227.108.131:/root/new"  // copy docker-compose.yml file to remote server
+        sh "ssh -o StrictHostKeyChecking=no root@64.227.108.131 '${makedir};${cd};${dockerCmd}'"  // running commands on remote server
         
     }
 }
