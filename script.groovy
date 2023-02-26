7c55dcd96a5f0347f1441a842dffa562e3480257
ankitraz <109873375+ankitraz@users.noreply.github.com>
2023-02-26 10:29:27 +0530

    remove unnecessary comments

--- script.groovy
+++ script.groovy
@@ -18,7 +18,7 @@ def Deploy(){
     echo "Deploying the application..."
     def makedir = "mkdir /root/new"  // creating a new directory on remote server
     def cd = "cd /root/new"  // changing directory to new directory on remote server where we copied docker-compose.yml file
-    def dockerCmd = "TAG=${env.TAG} REPO_NAME=${env.REPO_NAME} docker-compose up -d" // running docker-compose up command on remote server using TAG variable
+    def dockerCmd = "TAG=${env.TAG} REPO_NAME=${env.REPO_NAME} docker-compose up -d" // running docker-compose up command on remote server using TAG variable and REPO_NAME variable
     
     sshagent(['github-ssh-key']) {
         //copying docker-compose.yml file to remote server
