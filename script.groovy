0bdafefc372b7b8baa7f155cadda45d039ba4445
ankitraz <109873375+ankitraz@users.noreply.github.com>
2023-03-19 12:47:16 +0530

    updated credentials

--- script.groovy
+++ script.groovy
@@ -20,7 +20,7 @@ def Deploy(){
     def cd = "cd /root/new"  // changing directory to new directory on remote server where we copied docker-compose.yml file
     def dockerCmd = "TAG=${env.TAG} REPO_NAME=${env.REPO_NAME} docker-compose up -d" // running docker-compose up command on remote server using TAG variable and REPO_NAME variable
     
-    sshagent(['github-ssh-key']) {  // using  private ssh key of jenkins to connect to remote production server
+    sshagent(['jenkins-private-key']) {  // using  private ssh key of jenkins to connect to remote production server
         //copying docker-compose.yml file to remote server
         sh "scp docker-compose.yml ${env.SERVER_USER}@${env.SERVER_IP}:/root/new"  // copy docker-compose.yml file to remote server
         sh "ssh -o StrictHostKeyChecking=no ${env.SERVER_USER}@${env.SERVER_IP} '${makedir};${cd};${dockerCmd}'"  // running commands on remote server, you can add more commands here seperating them with semicolon
